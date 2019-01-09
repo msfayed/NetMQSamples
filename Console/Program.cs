45da8b9d0a039183c63fb7114493c4df3a51c6db
fayed <mohammed@fayedcom.com>
2019-01-09 12:29:07 +0400

    Add Console sample

---
+++ Console/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Core;
+using NetMQ.Sockets;
+using System.Threading;
+using System.Reflection;
+using System.IO;
+
+namespace Project2
+{
+    class Program
+    {
+        private const int mDelayTime = 1000;
+
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                if (args[0].ToLower().StartsWith("s")) // server 
+                {
+                    Console.WriteLine("Startting The Server .. ");
+
+                    using (var socket = new ResponseSocket("tcp://*:5555"))
+                    {
+                        Console.WriteLine("Server Started.");
+                        while (true)
+                        {
+                            //Thread.Sleep(mDelayTime);
+                            var rcvdMsg = socket.ReceiveFrameString(Encoding.UTF8);
+                            Console.WriteLine("Received: " + rcvdMsg);
+                            var replyMsg = "Message Received : " + rcvdMsg;
+                            Console.WriteLine("Sending : " + replyMsg + Environment.NewLine);
+                            socket.SendFrame(replyMsg);
+                        }
+                    }
+                }
+                else // Client
+                {
+                    var mID = args[0].Trim(); //DateTime.Now.ToString("HH-mm-ss-fff");
+                    Console.WriteLine("Startting The Client .. " + mID);
+
+
+                    using (var socket = new RequestSocket("tcp://127.0.0.1:5555"))
+                    {
+                        Console.WriteLine("Client Started.");
+                        while (true)
+                        {
+                            Thread.Sleep(mDelayTime);
+
+                            var replyMsg = "Hello from Client [" + mID + "] " + DateTime.Now.ToString("HH:mm:ss.fff");
+                            Console.WriteLine("Sending : " + replyMsg + Environment.NewLine);
+
+                            socket.SendFrame(replyMsg);
+                            var rcvdMsg = socket.ReceiveFrameString(Encoding.UTF8);
+                            Console.WriteLine("Received: " + rcvdMsg);
+                        }
+                    }
+
+
+
+                }
+
+            }
+            else
+            {
+                var mAppPath = new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath;
+
+                System.Diagnostics.Process.Start(mAppPath, "s");
+                System.Diagnostics.Process.Start(mAppPath, "c1");
+                System.Diagnostics.Process.Start(mAppPath, "c2");
+            }
+
+        }
+    }
+}
---
+++ Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("peoject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("peoject2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("707cc28d-a9e2-455d-bb97-3549354dc1a5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
